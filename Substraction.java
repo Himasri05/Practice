64e2e2ff351efe04c8a22ebe15f8906a9e7499e6
Himasri05 <himasriakula@gmail.com>
2025-01-24 22:42:57 +0530

    changed substraction again

--- Substraction.java
+++ Substraction.java
@@ -1,8 +1,9 @@
 public class Substraction {
     public static void main(String[] args){
-        int a=1;
+        int a=6;
         int b=5;
         int c=b-a;
         System.out.println(c);
+        System.out.println("Test");
     }
 }
